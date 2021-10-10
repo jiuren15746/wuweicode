b638c4f0e6994aded943b3fdd628493a860764df
will <jiuren@alibaba-inc.com>
2021-10-10 17:48:12 +0800

    可重入锁。

--- src/main/java/concurrency/IntReentranceLock.java
+++ src/main/java/concurrency/IntReentranceLock.java
@@ -4,29 +4,49 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static concurrency.PrintLog.printLog;
 
 /**
- * 可重入的锁。
+ * 可重入锁。仅使用CAS实现。
  */
 public class IntReentranceLock {
 
-    private AtomicInteger value = new AtomicInteger(0);
+    // 0表示锁空闲。大于零表示加锁次数。
+    private AtomicInteger lockCount = new AtomicInteger(0);
 
-    private Thread holdingThread = null;
+    // 持有锁的线程
+    private volatile Thread holdingThread = null;
 
     //--------
 
+    /**
+     * 阻塞式加锁。直到加锁成功，或者线程被中断，才返回。
+     *
+     * @throws InterruptedException
+     */
     public void lock() throws InterruptedException {
-        if (isHeldByCurrentThread()) {
-            value.incrementAndGet();
-            return;
-        }
+        for (int tryCount = 0;; tryCount++) {
+            // 尝试10次，休息一会
+            if (tryCount > 0 && tryCount % 10 == 0) {
+                Thread.sleep(randomValueWithin(3));
+            }
 
-        for (;;) {
-            if (value.compareAndSet(0, 1)) {
+            // 不能根据lockCount来判断锁状态。因为解锁时，先减count，再清空线程引用。
+            // 如果通过lockCount来判断状态，线程解锁时在这两步中间可能被另一个线程插入，导致清除线程引用时出错。
+            // 所以，必须根据解锁最后一步的特征，来判断状态。
+            if (null != holdingThread) {
+                // 被当前线程占用。重入加锁
+                if (isHeldByCurrentThread()) {
+                    lockCount.incrementAndGet();
+                    return;
+                } else {
+                    // 锁仍在被其他线程占用
+                    continue;
+                }
+            }
+
+            if (lockCount.compareAndSet(0, 1)) {
                 holdingThread = Thread.currentThread();
-                printLog("lock success. value=" + value.get());
+                printLog("lock success. value=" + lockCount.get());
                 return;
             }
-            Thread.sleep(1);
         }
     }
 
@@ -34,13 +54,21 @@ public class IntReentranceLock {
         if (!isHeldByCurrentThread()) {
             throw new IllegalMonitorStateException();
         }
-        value.decrementAndGet();
-        holdingThread = null;
-        printLog("unlock success, value=" + value.get());
+
+        // decrease lock count。if decrease to zero, set holdingThread = null
+        int newCount = lockCount.decrementAndGet();
+        if (0 == newCount) {
+            holdingThread = null;
+        }
+        printLog("unlock success, lockCount=" + newCount);
     }
 
     public boolean isHeldByCurrentThread() {
         return Thread.currentThread() == holdingThread;
     }
 
+    // 返回 [0, maxValue) 的随机值
+    private long randomValueWithin(long maxValue) {
+        return System.nanoTime() % maxValue;
+    }
 }
