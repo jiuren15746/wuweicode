1623ab69e3fda4d64736b5038aed3f6c5322eded
will <jiuren@alibaba-inc.com>
2022-01-03 19:28:54 +0800

    修bug

--- src/main/java/datastructure/linkedlist/LinkedListJiuren.java
+++ src/main/java/datastructure/linkedlist/LinkedListJiuren.java
@@ -1,19 +1,33 @@
 package datastructure.linkedlist;
 
+import org.testng.collections.Lists;
+
+import java.util.List;
+
 public class LinkedListJiuren {
     ListNode head;
     ListNode tail;
     // =============
 
+    // 从数组创建链表
+    static public LinkedListJiuren createList(int[] array) {
+        LinkedListJiuren list = new LinkedListJiuren();
+        for (int val : array) {
+            list.push(val);
+        }
+        return list;
+    }
+
     /**
      * 在头部插入新节点
      */
     public void push(int value) {
-        ListNode node = new ListNode(value);
-        node.next = head;
-        head = node;
-        if (tail == null) {
-            tail = head;
+        if (head == null) {
+            head = tail = new ListNode(value);
+        } else {
+            ListNode node = new ListNode(value);
+            node.next = head;
+            head = node;
         }
     }
 
@@ -27,7 +41,7 @@ public class LinkedListJiuren {
     // 在尾部插入节点
     public void append(int value) {
         if (head == null) {
-            tail = head = new ListNode(value);
+            head = tail = new ListNode(value);
         } else {
             tail = tail.next = new ListNode(value);
         }
@@ -46,4 +60,140 @@ public class LinkedListJiuren {
         if (node == null) return 0;
         return 1 + getLength_recursive(node.next);
     }
+
+    static ListNode getPrevious(ListNode head, ListNode node) {
+        ListNode i = head;
+        while (i != null && i.next != node) {
+            i = i.next;
+        }
+        return i;
+    }
+
+    static List<Integer> toList(ListNode head) {
+        List<Integer> result = Lists.newArrayList();
+        for (ListNode i = head; i != null; i = i.next) {
+            result.add(i.data);
+        }
+        return result;
+    }
+
+
+
+    static public ListNode[] quickSort(ListNode head, ListNode tail) {
+        int base = tail.data;
+        ListNode baseNode = tail;
+        ListNode pre = null;
+
+        for (ListNode i = head; i != baseNode; ) {
+            System.out.println(toList(head) + ", node=" + i.data);
+
+            if (i.data < base) {
+                pre = i;
+                i = i.next;
+                continue;
+            }
+            // 从链表移除
+            if (pre != null) { // 从中间移除
+                pre.next = i.next;
+            } else { // 从表头移除
+                head = i.next;
+            }
+            i.next = null;
+
+            // 放到链尾
+            tail = tail.next = i;
+
+            // 继续下一个节点
+            if (pre != null) {
+                i = pre.next;
+            } else {
+                i = head;
+            }
+        }
+        System.out.println(toList(head) + ", baseNode=" + base);
+
+        ListNode basePre = getPrevious(head, baseNode);
+        ListNode baseNext = baseNode.next;
+
+        // 对左半边递归调用
+        if (basePre != null && head != basePre) {
+            basePre.next = null;
+            ListNode[] leftResult = quickSort(head, basePre);
+            head = leftResult[0];
+            leftResult[1].next = baseNode;
+        }
+
+        // 对右半边递归调用
+        if (baseNext != null && baseNext != tail) {
+            ListNode[] rightResult = quickSort(baseNext, tail);
+            baseNode.next = rightResult[0];
+            tail = rightResult[1];
+        }
+        return new ListNode[]{head, tail};
+    }
+
+
+
+//    public void quickSort2(ListNode begin, ListNode end) {
+//        //判断为空，或者只有一个节点
+//        if (begin == null || end == null || begin == end) {
+//            return;
+//        }
+//        //从第一个节点和第一个节点的下个节点
+//        ListNode first = begin;
+//        ListNode second = begin.next;
+//        int nMidValue = begin.data;
+//
+//        //结束条件，second到最后了
+//        while (second != end.next && second != null) {
+//            if (second.data < nMidValue) {
+//                first = first.next;
+//                //判断一下，避免后面的数比第一个数小，不用换的局面
+//                if (first != second) {
+//                    int temp = first.data;
+//                    first.data = second.data;
+//                    second.data = temp;
+//                }
+//            }
+//            second = second.next;
+//        }
+//        //判断，有些情况是不用换的，提升性能
+//        if (begin != first) {
+//            int temp = begin.data;
+//            begin.data = first.data;
+//            first.data = temp;
+//        }
+//        //前部分递归
+//        quickSort2(begin, first);
+//        //后部分递归
+//        quickSort2(first.next, end);
+//    }
+
+
+    public void quickSort_swapValue(ListNode begin, ListNode end) {
+        if (begin == null || end == null || begin == end) {
+            return;
+        }
+
+        int base = begin.data;
+        ListNode p = begin;
+        for (ListNode i = begin; i != end.next; i = i.next) {
+            if (i.data < base) {
+                p = p.next;
+                swap(p, i);
+            }
+        }
+        swap(p, begin);
+
+        ListNode pre = getPrevious(begin, p);
+        quickSort_swapValue(begin, pre);
+        quickSort_swapValue(p.next, end);
+    }
+
+    private void swap(ListNode a, ListNode b) {
+        int temp = a.data;
+        a.data = b.data;
+        b.data = temp;
+    }
+
 }
--- src/test/java/datastructure/linkedlist/LinkedListJiurenTest.java
+++ src/test/java/datastructure/linkedlist/LinkedListJiurenTest.java
@@ -3,16 +3,24 @@ package datastructure.linkedlist;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.collections.Lists;
+
+import static datastructure.linkedlist.LinkedListJiuren.*;
 
 public class LinkedListJiurenTest {
     @Test
     public void test_push() {
         LinkedListJiuren list = new LinkedListJiuren();
         list.push(1);
-        list.push(3);
-        list.push(1);
         list.push(2);
-        list.push(1);
+        list.push(3);
+        list.push(4);
+        list.push(5);
+
+        Assert.assertNotNull(list.head);
+        Assert.assertNotNull(list.tail);
+        Assert.assertEquals(list.head.data, 5);
+        Assert.assertEquals(list.tail.data, 1);
 
         Assert.assertEquals(list.getLength(), 5);
         Assert.assertEquals(list.getLength_recursive(list.head), 5);
@@ -22,12 +30,63 @@ public class LinkedListJiurenTest {
     public void test_append() {
         LinkedListJiuren list = new LinkedListJiuren();
         list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+
+        Assert.assertNotNull(list.head);
+        Assert.assertNotNull(list.tail);
+        Assert.assertEquals(list.head.data, 1);
+        Assert.assertEquals(list.tail.data, 5);
+
+        Assert.assertEquals(list.getLength(), 5);
+        Assert.assertEquals(list.getLength_recursive(list.head), 5);
+
+        Assert.assertEquals(toList(list.head), Lists.newArrayList(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void test_getPrevious() {
+        LinkedListJiuren list = new LinkedListJiuren();
+        list.append(1);
         list.append(3);
         list.append(1);
         list.append(2);
         list.append(1);
 
-        Assert.assertEquals(list.getLength(), 5);
-        Assert.assertEquals(list.getLength_recursive(list.head), 5);
+        Assert.assertEquals(getPrevious(list.head, list.head.next), list.head);
+        Assert.assertEquals(getPrevious(list.head, list.tail).data, 2);
+        Assert.assertEquals(getPrevious(list.head, list.head), null);
+    }
+
+
+    @Test
+    public void test_quicksort() {
+        LinkedListJiuren list = new LinkedListJiuren();
+        list.append(6);
+        list.append(5);
+        list.append(2);
+        list.append(7);
+        list.append(1);
+        list.append(3);
+        list.append(9);
+        list.append(8);
+        list.append(4);
+
+        ListNode[] result = quickSort(list.head, list.tail);
+        ListNode head = result[0];
+
+        System.out.println(toList(head));
+    }
+
+
+    @Test
+    public void test_quicksort_swapValue() {
+        LinkedListJiuren list = LinkedListJiuren.createList(new int[]{5,4,8,7,3,6,5,2});
+
+        list.quickSort_swapValue(list.head, list.tail);
+
+        System.out.println(toList(list.head));
     }
 }
