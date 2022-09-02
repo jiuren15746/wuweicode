c3b67dc0fc26bc8f71c1dbd06ce9e5330292a215
winston <winston@cmexpro.com>
2022-09-02 17:43:21 +0800

    skipListV5 test.

--- src/test/java/datastructure/skiplist/v5/SkipListV5ImplTest.java
+++ src/test/java/datastructure/skiplist/v5/SkipListV5ImplTest.java
@@ -9,6 +9,61 @@ import org.testng.annotations.Test;
 
 public class SkipListV5ImplTest {
 
+    @Test
+    public void getFirst_emptyList() {
+        SkipListV5<Object> skipList1 = createSkipList(true);
+        assertNull(skipList1.getFirst());
+
+        SkipListV5<Object> skipList2 = createSkipList(false);
+        assertNull(skipList2.getFirst());
+    }
+
+    @Test
+    public void insert_twoSameKey() {
+        SkipListV5<Object> skipList1 = createSkipList(true);
+        // 插入两个相同的key
+        long key = 12345678L;
+        assertEquals(skipList1.insert(key, new Object()), true);
+        assertEquals(skipList1.insert(key, new Object()), false);
+
+        SkipListV5<Object> skipList2 = createSkipList(false);
+        // 插入两个相同的key
+        assertEquals(skipList2.insert(key, new Object()), true);
+        assertEquals(skipList2.insert(key, new Object()), false);
+    }
+
+    @Test
+    public void asc_insert_1_to_100() {
+        SkipListV5<Object> skipList = createSkipList(true);
+
+        for (long key = 1; key <= 100; ++key) {
+            skipList.insert(key, "" + key);
+        }
+        assertEquals(skipList.size(), 100);
+
+        for (long key = 1; key <= 100; ++key) {
+            assertEquals(skipList.getFirst(), "" + key);
+            skipList.removeFirst();
+        }
+        assertEquals(skipList.size(), 0);
+    }
+
+    @Test
+    public void desc_insert_1_to_100() {
+        SkipListV5<Object> skipList = createSkipList(false);
+
+        for (long key = 1; key <= 100; ++key) {
+            skipList.insert(key, "" + key);
+        }
+        assertEquals(skipList.size(), 100);
+
+        for (long key = 100; key >= 1; --key) {
+            assertEquals(skipList.getFirst(), "" + key);
+            skipList.removeFirst();
+        }
+        assertEquals(skipList.size(), 0);
+    }
+
     private SkipListV5<Object> createSkipList(boolean asc) {
         Comparator<Long> comparator = new Comparator<Long>() {
             @Override
@@ -23,49 +78,5 @@ public class SkipListV5ImplTest {
             }
         };
         return new SkipListV5Impl<>(comparator);
-    }
-
-    @Test
-    public void getFirst_emptyList() {
-        SkipListV5<Object> skipList1 = createSkipList(true);
-        assertNull(skipList1.getFirst());
-
-        SkipListV5<Object> skipList2 = createSkipList(false);
-        assertNull(skipList2.getFirst());
-
-
-
-        // 插入1-100
-    }
-
-    @Test
-    public void insert_twoSameKey() {
-        SkipListV5<Object> skipList1 = createSkipList(true);
-        // 插入两个相同的key
-        long key = 12345678L;
-        assertEquals(skipList1.insert(key, new Object()), true);
-        assertEquals(skipList1.insert(key, new Object()), false);
-    }
-
-    @Test
-    public void insert_1_to_100() {
-        SkipListV5<Object> skipList1 = createSkipList(true);
-
-        for (long key = 1; key <= 100; ++key) {
-            skipList1.insert(key, "" + key);
-        }
-        assertEquals(skipList1.size(), 100);
-
-        for (long key = 1; key <= 100; ++key) {
-            assertEquals(skipList1.getFirst(), "" + key);
-            skipList1.removeFirst();
-        }
-        assertEquals(skipList1.size(), 0);
-    }
-
-
-    @Test
-    public void test_desc() {
-
     }
 }
