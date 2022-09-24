bead83e5739c1505a0b931a6f78c50a8fed72b25
wuwei <jiuren15746@gmail.com>
2022-09-24 18:45:26 +0800

    binary search.

--- src/main/java/datastructure/array/algo/binarysearch/BinarySearch.java
+++ src/main/java/datastructure/array/algo/binarysearch/BinarySearch.java
@@ -6,24 +6,24 @@ public class BinarySearch {
      * 返回的位置可能在数组下标范围之外。
      */
     public static int searchPos(char[] array, int start, int end, char target) {
-        // 这里的判断是为了提高性能。如果target在指定范围之外，可以直接返回。不加的话走下面的逻辑，也能正确返回，只是效率不高。
-        if (target < array[start]) {
-            return start;
-        }
-        if (target > array[end]) {
-            return end + 1;
-        }
         while (start <= end) {
+            // 每次调整范围后，target与新范围的左右边界值比较，非常重要！！！
+            if (target <= array[start]) {
+                return start;
+            }
+            if (target > array[end]) {
+                return end + 1;
+            }
             int midPos = (start + end) >> 1;
-            if (array[midPos] == target) {
+            int diff = target - array[midPos];
+            if (diff == 0) {
                 return midPos;
-            } else if (array[midPos] < target) {
+            } else if (diff > 0) {
                 start = midPos + 1;
             } else {
                 end = midPos - 1;
             }
         }
-        // start > end，返回start。举例思考一下。
-        return start;
+        throw new RuntimeException("Impossible");
     }
 }
