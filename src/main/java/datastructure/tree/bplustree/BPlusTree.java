26ec360c94037817efae93d3352dd13ae691250e
wuwei <jiuren15746@gmail.com>
2022-09-24 20:39:05 +0800

    bplus tree.

--- src/main/java/datastructure/tree/bplustree/BPlusTree.java
+++ src/main/java/datastructure/tree/bplustree/BPlusTree.java
@@ -1,15 +1,36 @@
 package datastructure.tree.bplustree;
-import datastructure.tree.bplustree.Node.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+
 
 public class BPlusTree {
+    @Getter
+    private final int maxDegree;
 
-    int m;
-    InternalNode root;
-    LeafNode firstLeaf;
+    @Getter
+    protected Node root;
     //========
 
-    public BPlusTree(int m) {
-        this.m = m;
-        this.root = null;
+    public BPlusTree(int maxDegree) {
+        this.maxDegree = maxDegree;
+        this.root = new Node(this, true);
+    }
+
+
+
+    public static void main(String[] args) {
+        final int maxDegree = 4;
+        BPlusTree tree = new BPlusTree(maxDegree);
+        assertEquals(tree.getRoot().getDegree(), 0);
+
+        long[] array = {8, 20, 10, 6, 30};
+        for (long key : array) {
+            tree.getRoot().insert(key, "" + key);
+        }
+
+        assertEquals(tree.getRoot().getDegree(), 2);
     }
 }
--- src/main/java/datastructure/tree/bplustree/Node.java
+++ src/main/java/datastructure/tree/bplustree/Node.java
@@ -1,80 +1,128 @@
 package datastructure.tree.bplustree;
 
-import java.util.Arrays;
+import lombok.Data;
 
+/**
+ * 中间节点和叶子节点结构相同。
+ */
+@Data
 class Node {
-    InternalNode parent;
+    private final BPlusTree tree;
+    private final boolean isLeaf;
 
-    static class InternalNode extends Node {
-        int maxDegree;
-        int minDegree;
-        // degree表示有多少个孩子? todo
-        int degree;
+//    private final int maxDegree;
+//    private final int minDegree;
+    /**
+     * degree表示有多少个孩子? todo
+     */
+    private int degree;
 
-        // 中间节点也用链表连接
-        InternalNode pre;
-        InternalNode next;
+    private long[] keys;
+    /**
+     * 如果是中间节点，元素类型为Node。如果是叶子节点，元素类型是Object。
+     */
+    private Object[] childrenOrData;
 
-        long[] keys;
-        Node[] children;
+    private Node pre;
+    private Node next;
+    private Node parent;
+    //========
+
+    public Node(BPlusTree tree, boolean isLeaf) {
+        this.tree = tree;
+        this.isLeaf = isLeaf;
+        this.degree = 0;
+        // maxDegree+1是为了便于split
+        this.keys = new long[tree.getMaxDegree() + 1];
+        this.childrenOrData = new Object[tree.getMaxDegree() + 1];
     }
 
+    /**
+     * 在array指定长度范围内，二分查找value所在的位置。如果value不在数组中，返回应该插入的位置。start和end表示搜索范围，左右都包含。
+     * 返回的位置可能在数组下标范围之外。
+     */
+    public int binarySearch(long target) {
+        int start = 0;
+        int end = degree - 1;
 
-    static class LeafNode extends Node {
-        int maxSize;
-        int minSize;
-        int size = 0;
-        LeafNode pre;
-        LeafNode next;
-        DictionaryPair[] dictionary;
-
-        public LeafNode(int m, DictionaryPair dp) {
-            this.maxSize = m;
-            this.minSize = (int) Math.ceil(m / 2);
-            this.dictionary = new DictionaryPair[m];
-            this.insert(dp);
-        }
-
-        public LeafNode(int m, DictionaryPair[] dps, InternalNode parent) {
-            this.parent = parent;
-            this.maxSize = m;
-            this.minSize = (int) Math.ceil(m / 2);
-            this.dictionary = dps;
-            for (int i = 0; i < dps.length; i++) {
-                if (dps[i] != null) {
-                    size++;
-                } else {
-                    break;
-                }
+        while (start <= end) {
+            // 每次调整范围后，target与新范围的左右边界值比较，非常重要！！！
+            if (target <= keys[start]) {
+                return start;
+            }
+            if (target > keys[end]) {
+                return end + 1;
+            }
+            int midPos = (start + end) >> 1;
+            long diff = target - keys[midPos];
+            if (diff == 0) {
+                return midPos;
+            } else if (diff > 0) {
+                start = midPos + 1;
+            } else {
+                end = midPos - 1;
             }
         }
-
-        public boolean insert(DictionaryPair dp) {
-            if (isFull()) {
-                return false;
-            }
-            dictionary[size++] = dp;
-            Arrays.sort(dictionary, 0, size);
-            return true;
-        }
-
-        public boolean isFull() {
-            return size == maxSize;
-        }
+        throw new RuntimeException("Impossible");
     }
 
-    static class DictionaryPair implements Comparable<DictionaryPair> {
-        long key;
-        double value;
-
-        public DictionaryPair(long key, double value) {
-            this.key = key;
-            this.value = value;
+    /**
+     *
+     * @param key
+     * @param value
+     */
+    public void insert(long key, Object value) {
+        if (degree == 0) {
+            keys[0] = key;
+            childrenOrData[0] = value;
+            degree++;
+            return;
         }
 
-        @Override
-        public int compareTo(DictionaryPair o) {
-            return key == o.key ? 0 : (key < o.key ? -1 : 1);
+        final int pos = binarySearch(key);
+
+        // 插入到结尾后面
+        if (pos == degree) {
+            keys[degree] = key;
+            childrenOrData[degree] = value;
+        } else {
+            System.arraycopy(keys, pos, keys, pos + 1, degree - pos);
+            System.arraycopy(childrenOrData, pos, childrenOrData, pos + 1, degree - pos);
+            keys[pos] = key;
+            childrenOrData[pos] = value;
         }
+        degree++;
+
+        splitIfNecessary();
     }
+
+
+    private void splitIfNecessary() {
+        if (degree <= tree.getMaxDegree()) {
+            return;
+        }
+
+        int newDegree = degree >> 1;
+        int newNodeDegree = degree - newDegree;
+
+        // split new Node
+        Node newNode = new Node(tree, isLeaf);
+        System.arraycopy(keys, newDegree, newNode.keys, 0, newNodeDegree);
+        System.arraycopy(childrenOrData, newDegree, newNode.childrenOrData, 0, newNodeDegree);
+        newNode.degree = newNodeDegree;
+        this.degree = newDegree;
+
+        // 维护pre, next, parent
+        this.next = newNode;
+        newNode.pre = this;
+        newNode.parent = parent;
+
+        if (null == parent) {
+            newNode.parent = parent = new Node(tree, false);
+            tree.root = parent;
+            parent.insert(keys[0], this);
+        }
+        parent.insert(newNode.keys[0], newNode);
+    }
+
 }
