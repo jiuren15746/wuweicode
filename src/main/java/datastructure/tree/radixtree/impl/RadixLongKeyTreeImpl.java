3fa78e5fda9917f5392dcb2fda24061d427d1163
wuwei <jiuren15746@gmail.com>
2022-10-04 10:44:29 +0800

    radix tree.

--- src/main/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImpl.java
+++ src/main/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImpl.java
@@ -1,8 +1,8 @@
 package datastructure.tree.radixtree.impl;
 
-import static org.testng.Assert.assertTrue;
-
 import datastructure.tree.radixtree.RadixLongKeyTree;
+
+import static org.testng.Assert.assertTrue;
 
 /**
  * Radix tree的高度是固定的。对于一个long型key，每6bit为一组，拆分为N组N=11，树的最大高度就为N（包括N层中间节点，一层叶子节点）。
@@ -16,61 +16,78 @@ import datastructure.tree.radixtree.RadixLongKeyTree;
 public class RadixLongKeyTreeImpl<V> implements RadixLongKeyTree<V> {
 
     private static final int MAX_CHILD_SIZE = 64;
-    // 中间节点的height范围[0,10]。叶子节点的height=11
-    private static final int MAX_HEIGHT = 11;
+
+    /**
+     * 根节点height为零。向下递增。叶子节点的height=10
+     */
+    private static final int MAX_HEIGHT = 10;
+
     private final Node<V> root;
     //========
 
     private static class Node<V> {
-        // 节点高度，根节点高度为0. 实际用byte类型也可以。
-        private int height;
+
         private boolean isLeaf;
-        // 如果是中间节点，指向下级节点数组。如果是叶子节点，指向value。
-        private Object childrenOrValue;
+
+        /**
+         * 节点高度，根节点高度为0. 实际用byte类型也可以。用于截图long类型key的二进制中的一段。
+         */
+        private int height;
+
+        /**
+         * 如果是中间节点，指向下级节点数组。如果是叶子节点，指向value。
+         */
+        private Object[] values;
         //========
-        private Node<V> addChild(byte routeKey, V value) {
+
+        public Node(int height) {
+            this.height = height;
+            this.isLeaf = (height == MAX_HEIGHT);
+            values = new Object[isLeaf ? 16 : 64];
+        }
+
+        /**
+         * 用于给中间节点创建下级节点。
+         * @param routeKey
+         * @return
+         */
+        private Node<V> addChild(byte routeKey) {
             assertTrue(!isLeaf);
-            Node<V> child = new Node<>();
-            child.height = this.height + 1;
-            child.isLeaf = (child.height == MAX_HEIGHT);
-            child.childrenOrValue = child.isLeaf ? value : new Node[MAX_CHILD_SIZE];
-            ((Node[]) this.childrenOrValue)[routeKey] = child;
+            Node<V> child = new Node<>(this.height + 1);
+            values[routeKey] = child;
             System.out.println("add child, routeKey=" + routeKey + ", height=" + child.height);
             return child;
         }
     }
 
     public RadixLongKeyTreeImpl() {
-        root = new Node<>();
-        root.height = 0;
-        root.isLeaf = false;
-        root.childrenOrValue = new Node[MAX_CHILD_SIZE];
+        root = new Node<>(0);
     }
 
     @Override
     public V find(long key) {
-        Node<V> current = root;
-        for (int i = 0; i < MAX_HEIGHT && null != current; ++i) {
-            // 取低六位
-            byte routeKey = (byte) (key & 0x3F);
-            current = ((Node[]) current.childrenOrValue)[routeKey];
-            key = (key >>> 6);
-        }
-        return (null != current) ? (V) current.childrenOrValue : null;
+        Node<V> leaf = findLeafNode(key);
+        byte routeKey = (byte) (key & 0x0F);
+        return (V) leaf.values[routeKey];
     }
 
     @Override
     public void put(long key, V value) {
+        Node<V> leaf = findLeafNode(key);
+        byte routeKey = (byte) (key & 0x0F);
+        leaf.values[routeKey] = value;
+    }
+
+    private Node<V> findLeafNode(long key) {
         Node<V> current = root;
         for (int i = 0; i < MAX_HEIGHT; ++i) {
-            byte routeKey = (byte) (key & 0x3F);
-            Node<V> child = ((Node[]) current.childrenOrValue)[routeKey];
-            if (null == child) {
-                child = current.addChild(routeKey, value);
+            byte routeKey = (byte)((key >> (64 - (i + 1) * 6)) & 0x3F);
+            System.out.println("height=" + i + ", routeKey=" + routeKey);
+            if (null == current.values[routeKey]) {
+                current = current.addChild(routeKey);
             }
-            current = child;
-            key = (key >>> 6);
         }
+        return current;
     }
 
 }
