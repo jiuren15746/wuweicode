eceb8b0623c3cd938440aed88f0cc8cac165eedf
wuwei <jiuren15746@gmail.com>
2022-10-04 11:33:06 +0800

    radix tree.

--- src/main/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImpl.java
+++ src/main/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImpl.java
@@ -9,23 +9,15 @@ import static org.testng.Assert.assertTrue;
  * 因为每次路由的决策数字有6bit，所以一个中间节点有2^6即64个子节点。
  * 所有值都挂在叶子节点上。
  *
+ * 经过测试，100w个key的tree，Radix tree查找100w个key。只需要60ms。比起skipList好非常多。
+ *
  * todo 叶子节点以双向链表串联
  * todo 保存最小和最大叶子节点
+ * @author wuwei
  */
 public class RadixLongKeyTreeImpl<V> implements RadixLongKeyTree<V> {
 
-    private static final int MAX_CHILD_SIZE = 64;
-
-    /**
-     * 根节点height为零。向下递增。叶子节点的height=10
-     */
-    private static final int MAX_HEIGHT = 10;
-
-    private final Node<V> root;
-    //========
-
     private static class Node<V> {
-
         private boolean isLeaf;
 
         /**
@@ -52,12 +44,19 @@ public class RadixLongKeyTreeImpl<V> implements RadixLongKeyTree<V> {
          */
         private Node<V> addChild(byte routeKey) {
             assertTrue(!isLeaf);
-            Node<V> child = new Node<>(this.height + 1);
+            Node<V> child = new Node<>(height + 1);
             values[routeKey] = child;
-            System.out.println("add child, routeKey=" + routeKey + ", height=" + child.height);
             return child;
         }
     }
+
+    /**
+     * 根节点height为零。向下递增。叶子节点的height=10
+     */
+    private static final int MAX_HEIGHT = 10;
+
+    private final Node<V> root;
+    //========
 
     public RadixLongKeyTreeImpl() {
         root = new Node<>(0);
@@ -80,7 +79,8 @@ public class RadixLongKeyTreeImpl<V> implements RadixLongKeyTree<V> {
     private Node<V> findLeafNode(long key) {
         Node<V> current = root;
         for (int i = 0; i < MAX_HEIGHT; ++i) {
-            byte routeKey = (byte)((key >> (64 - (i + 1) * 6)) & 0x3F);
+            long shiftKey = key >> ((64 - (i + 1) * 6));
+            byte routeKey = (byte)(shiftKey & 0x3F);
             current = (null != current.values[routeKey]) ? (Node)current.values[routeKey] : current.addChild(routeKey);
         }
         return current;
--- src/test/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImplTest.java
+++ src/test/java/datastructure/tree/radixtree/impl/RadixLongKeyTreeImplTest.java
@@ -2,14 +2,35 @@ package datastructure.tree.radixtree.impl;
 
 import static org.testng.Assert.assertEquals;
 
+import datastructure.skiplist.sparseskiplist.SkipListV6;
 import org.testng.annotations.Test;
 
 public class RadixLongKeyTreeImplTest {
     private RadixLongKeyTreeImpl<String> tree = new RadixLongKeyTreeImpl<>();
 
     @Test
+    public void testFind_100w() {
+        for (int i = 0; i < 5; ++i) {
+            RadixLongKeyTreeImpl<String> tree = new RadixLongKeyTreeImpl<>();
+
+            for (long key = 1; key <= 1000000L; ++key) {
+                tree.put(key, "" + key);
+            }
+
+            long start = System.currentTimeMillis();
+            for (long key = 1; key <= 1000000L; ++key) {
+                assertEquals(tree.find(key), "" + key);
+            }
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("duration=" + duration + " ms");
+        }
+    }
+
+
+    @Test
     public void testPutAndFind_KeyGreaterThanZero() {
         long key = System.nanoTime();
+        System.out.println("key=" + key + ", binary=" + longToBinary(key));
         tree.put(key, "" + key);
         assertEquals(tree.find(key), "" + key);
     }
@@ -40,5 +61,21 @@ public class RadixLongKeyTreeImplTest {
         long key = Long.MAX_VALUE;
         tree.put(key, "" + key);
         assertEquals(tree.find(key), "" + Long.MAX_VALUE);
+    }
+
+    private static String longToBinary(long key) {
+        String str = Long.toBinaryString(key);
+        for (int i = 64 - str.length(); i >= 1; --i) {
+            str = "0" + str;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); ++i) {
+            if (i > 0 && i % 6 == 0) {
+                sb.append(",");
+            }
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
     }
 }
